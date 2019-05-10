a748d2c635bafd29cc2a4d4cadf18bf9ff019c8d
Marc Romera Rodriguez <marcromera3@gmail.com>
2019-05-10 13:55:49 +0200

    Classe enemigo creada
    Imagenes enemigos implementadas

--- core/src/com/mygdx/game/Enemy1.java
+++ core/src/com/mygdx/game/Enemy1.java
@@ -1,4 +1,46 @@
 package com.mygdx.game;
 
-public class Enemy1 {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+import java.util.Random;
+
+public class Enemy1 extends Actor{
+    private int posX;
+    private int posY;
+    private final float maxH = Gdx.graphics.getHeight();
+    private final float maxW = Gdx.graphics.getWidth();
+    private Texture texture;
+    private Rectangle bounds;
+
+    private boolean hasCollided;
+
+    public void Act()
+    {
+        posX--;
+        bounds.setPosition(posX,posY);
+    }
+   private Random random = new Random();
+
+    public Enemy1(Texture newTexture)
+    {
+        texture = newTexture;
+        posX = (int)maxW;
+        posY = random.nextInt((int)maxH);
+
+        bounds = new Rectangle(posX, posY, texture.getWidth(), texture.getHeight());
+
+
+    }
+
+    public void setPosX(int newPosX){posX = newPosX;}
+    public int getPosX(){return posX;}
+
+    public void setPosY(int newPosY){posY = newPosY;}
+    public int getPosY(){return posY;}
+
+    public void setTexture(Texture newTexture){texture = newTexture;}
+    public Texture getTexture(){return texture;}
 }
