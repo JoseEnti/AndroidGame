a07df4de45ef92bf39df46406cec3965a7bc0975
Jose <jose.ojalvo@enti.cat>
2019-05-17 10:47:52 +0200

    V0.1

--- core/src/com/mygdx/game/MainMenu.java
+++ core/src/com/mygdx/game/MainMenu.java
@@ -7,31 +7,75 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.utils.Align;
+import com.mygdx.utils.UIFactory;
 
 public class MainMenu extends AbstractScreen
 {
 	private SpriteBatch batch;
-	private Texture ship;
-	private Texture skyBox;
-	private Animation<TextureRegion> animation;
-	private Shape player;
-	private float elapsed;
+	private Texture background;
+	private Texture playButton;
+	private Texture exitButton;
+	private Stage mainMenu;
+	private ImageButton playBtn;
+	private ImageButton exitBtn;
 
 	public MainMenu()
 	{
-		//Asignar texturas
+		//Initialise level and textures
+		mainMenu = new Stage();
+		background = new Texture(Gdx.files.internal("background.jpg"));
+		playButton = new Texture(Gdx.files.internal("playbutton.png"));
+		exitButton = new Texture(Gdx.files.internal("exitbutton.png"));
 	}
 
 	@Override
 	public void buildStage()
 	{
-		InputProcessor p = new InputProcessor(player);
-		Gdx.input.setInputProcessor(p);
+		//Assign actors to the stage
+		Image bg = new Image(background);
+
+		playBtn = UIFactory.createButton(playButton);
+		playBtn.setPosition(getWidth() / 2, 120.f, Align.center);
+
+		exitBtn = UIFactory.createButton(exitButton);
+		exitBtn.setPosition(getWidth() / 2, 60.f, Align.center);
+
+		playBtn.addListener(UIFactory.createListener(ScreenEnum.GAME));
+
+		exitBtn.addListener(
+				new InputListener()
+				{
+					@Override
+					public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+						Gdx.app.exit();
+						return false;
+					}
+				});
+
+		mainMenu.addActor(bg);
+		mainMenu.addActor(playBtn);
+		mainMenu.addActor(exitBtn);
+		}
+
+	@Override
+	public void render(float delta)
+	{
+		mainMenu.act();
+		mainMenu.draw();
 	}
 
 	@Override
-	public void dispose ()
+	public void dispose()
 	{
-
+		super.dispose();
+		background.dispose();
+		playButton.dispose();
+		exitButton.dispose();
 	}
 }
--- core/src/com/mygdx/utils/CreateGame.java
+++ core/src/com/mygdx/utils/CreateGame.java
@@ -1,12 +1,14 @@
 package com.mygdx.utils;
 
 import com.badlogic.gdx.Game;
+import com.mygdx.game.ScreenEnum;
 
 public class CreateGame extends Game
 {
     @Override
     public void create()
     {
-
+        ScreenManager.getInstance().initialize(this);
+        ScreenManager.getInstance().showScreen(ScreenEnum.MAIN_MENU);
     }
 }
